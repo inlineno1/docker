46cc8ba0df443898cc40e8959df159ca7b02bce4
inlineno1 <inlineno1@gmail.com>
2018-05-17 14:09:53 +0900

    mariadb port mapping add

--- docker-shell-yona_mariadb/yona-db-run.sh
+++ docker-shell-yona_mariadb/yona-db-run.sh
@@ -2,5 +2,6 @@
 sudo docker run --name yona-db --privileged \
 -v ${PWD}/db/conf:/etc/mysql/conf.d \
 -v ${PWD}/db/data:/var/lib/mysql \
+-p=3306:3306
 -e TZ=Asia/Seoul \
 -e MYSQL_DATABASE=yona -e MYSQL_USER=yona -e MYSQL_PASSWORD=yona -e MYSQL_ROOT_PASSWORD=maria -d mariadb:10.2.14
